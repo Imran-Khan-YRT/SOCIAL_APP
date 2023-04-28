b02e91468e386c9038cbfd2ae16da7662ba2ba33
imrankhan <imran.khan@marginedge.com>
2023-04-28 18:19:20 +0600

    fix project name colour isuue

--- client/src/scenes/navbar/index.jsx
+++ client/src/scenes/navbar/index.jsx
@@ -50,9 +50,8 @@ const Navbar = () => {
       <FlexBetween gap="1.75rem">
         <Typography
           fontWeight="bold"
-          fontSize="clamp(1rem,2rem,2.25rem"
+          fontSize="clamp(1rem,2rem,2.25rem)"
           color="primary"
-          backgroundColor="#00353F"
           onClick={() => navigate("/home")}
           sx={{
             "& :hover": {
