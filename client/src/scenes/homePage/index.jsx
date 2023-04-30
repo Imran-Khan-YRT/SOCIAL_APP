6cc122e30d437e1a7943159358f831f27561ef98
imrankhan <imran.khan@marginedge.com>
2023-04-30 16:58:50 +0600

    add friend list section

--- client/src/scenes/homePage/index.jsx
+++ client/src/scenes/homePage/index.jsx
@@ -5,6 +5,7 @@ import MyPostWidget from "scenes/widgets/MyPostWidget";
 import PostsWidget from "scenes/widgets/PostsWidget";
 import UserWidget from "scenes/widgets/UserWidget";
 import AdvertWidget from "scenes/widgets/AdvertWidget"
+import FriendListWidget from "scenes/widgets/FriendListWidget";
 
 const HomePage = () => {
   const isNonMobileScreen = useMediaQuery("(min-width:1000px)");
@@ -34,6 +35,8 @@ const HomePage = () => {
         {isNonMobileScreen && (
           <Box flexBasis="26%">
             <AdvertWidget />
+            <Box m="2rem 0" />
+            <FriendListWidget userId={_id} />
           </Box>
         )}
       </Box>
